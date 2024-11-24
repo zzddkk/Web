456d160f81a2b2de8cd13e83a468946e7fc46efe
Your Name <you@example.com>
2024-11-24 21:19:21 +0800

    Part 1 b 1.5

--- part1/b/part1/src/App.jsx
+++ part1/b/part1/src/App.jsx
@@ -1,31 +1,33 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <h1>{course}</h1>
+      <h1>{course.name}</h1>
       <p>
-        {parts[0].name} {parts[0].exercises}
+        {course.parts[0].name} {course.parts[0].exercises}
       </p>
       <p>
-        {parts[1].name} {parts[1].exercises}
+        {course.parts[1].name} {course.parts[1].exercises}
       </p>
       <p>
-        {parts[2].name} {parts[2].exercises}
+        {course.parts[2].name} {course.parts[2].exercises}
       </p>
     </div>
   )
