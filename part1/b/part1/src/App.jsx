4a188421f01e0765b486d709ee1377facd859948
Your Name <you@example.com>
2024-11-26 16:57:48 +0800

    Part 1 c

--- part1/b/part1/src/App.jsx
+++ part1/b/part1/src/App.jsx
@@ -1,36 +1,47 @@
+import React, { useState } from 'react'
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const [counter, setCounter] = useState(0)
+  const Display = (props) => {
+    return (
+      <div>{props.counter}</div>
+    )
+  }
+  const Button = (props) => {
+    return (
+      <button onClick={props.onClick}>
+        {props.text}
+      </button>
+    )
+  }
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => { 
+
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
   }
 
   return (
     <div>
-      <h1>{course.name}</h1>
-      <p>
-        {course.parts[0].name} {course.parts[0].exercises}
-      </p>
-      <p>
-        {course.parts[1].name} {course.parts[1].exercises}
-      </p>
-      <p>
-        {course.parts[2].name} {course.parts[2].exercises}
-      </p>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
     </div>
   )
-}
+} 
 
-export default App
+export default App;
