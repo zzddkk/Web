fb925cc7c249c07908de15ec5bacf51483836f81
Your Name <you@example.com>
2024-11-28 14:10:33 +0800

    Part 1 d 1.6

---
+++ part1/d/part1/src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+
+const Button = ({ onClick, text }) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button text='good' onClick={() => setGood(good+1)} />
+      <Button text='neutral' onClick={() => setNeutral(neutral+1)} />
+      <Button text='bad' onClick={() => setBad(bad+1)} />
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+export default App
