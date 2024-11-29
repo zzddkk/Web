e0ac8cead5efcfa6cfa869079f5b971c15c04dca
Your Name <you@example.com>
2024-11-29 14:26:53 +0800

    Part 1 d 1.8

--- part1/d/part1/src/App.jsx
+++ part1/d/part1/src/App.jsx
@@ -8,6 +8,20 @@ const Button = ({ onClick, text }) => {
   )
 }
 
+const Statistics = (props) => {
+  const { good, neutral, bad } = props
+  return (
+    <div>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {good + bad + neutral}</p>
+      <p>average {(good - bad) / (good + bad + neutral)}</p>
+      <p>positive {good / (good + bad + neutral) * 100} %</p>
+    </div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -19,12 +33,7 @@ const App = () => {
       <Button text='neutral' onClick={() => setNeutral(neutral+1)} />
       <Button text='bad' onClick={() => setBad(bad+1)} />
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + bad + neutral}</p>
-      <p>average {(good - bad) / (good + bad + neutral)}</p>
-      <p>positive {good / (good + bad + neutral) * 100} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
