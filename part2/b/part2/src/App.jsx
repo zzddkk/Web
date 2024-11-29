657e6a8e41d2ab4f0065b1a08efb1caab5a0a115
Your Name <you@example.com>
2024-11-30 00:57:17 +0800

    Part 2 b 2.6

---
+++ part2/b/part2/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react"
+
+const App = () => {
+  const [persons, setPersons] = useState([{name : 'Arto Hellas'}])
+  const [newName, setNewName] = useState('')
+
+
+  const addperson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+    setPersons(persons.concat(personObject))
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+
+  return(
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addperson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+          <input type="submit" value="add"/>
+        </div>
+          
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      </div>
+    </div>
+  )
+} 
+
+export default App
